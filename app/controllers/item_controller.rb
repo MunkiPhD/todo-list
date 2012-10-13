ef47125363794b1b40d9008c1d1f1bdb220a933c
MunkiPhD <munkiphd@gmail.com>
2012-10-13 13:38:17 -0400

    added item tests

--- app/controllers/item_controller.rb
+++ app/controllers/item_controller.rb
@@ -7,6 +7,7 @@ class ItemController < ApplicationController
     respond_with(@items)
   end
 
+
   def show
     @item = current_user.items.find_by_id(params[:id])
     if @item.nil?
@@ -16,9 +17,11 @@ class ItemController < ApplicationController
     end
   end
 
+
   def new
     @item = Item.new
   end
+
 
   def create
     @item = current_user.items.build(params[:item])
@@ -30,13 +33,39 @@ class ItemController < ApplicationController
     end
   end
 
+
   def edit
+    @item = current_user.items.find_by_id(params[:id])
+     unless @item.nil?
+      respond_with(@item)
+     else 
+       redirect_to item_index_path
+     end
   end
+
 
   def update
+    @item = current_user.items.find_by_id(params[:id])
+
+    unless @item.nil?
+      if @item.update_attributes(params[:item])
+        redirect_to @item
+      else
+        render :edit
+      end
+    else
+      redirect_to item_index_path
+    end
   end
+
 
   def destroy
+    @item = current_user.items.find_by_id(params[:id])
+    unless @item.nil? 
+      @item.destroy
+      redirect_to item_index_path
+    else
+      redirect_to item_index_path
+    end
   end
-
 end
--- spec/controllers/item_controller_spec.rb
+++ spec/controllers/item_controller_spec.rb
@@ -61,6 +61,28 @@ describe ItemController do
     end
   end
 
+  describe "GET 'edit'" do
+    before(:each) do
+      @item = FactoryGirl.create(:item)
+    end
+
+    it "assigns the requested item to @item" do
+      get :edit, id: @item
+      assigns(:item).should eq @item
+    end
+
+    it "redirects the user on an item that doesnt belongs to the current user" do
+      user = FactoryGirl.create(:user_with_items)
+      get :edit, id: user.items.last
+      response.should redirect_to item_index_path
+    end
+
+    it "renders the edit template" do
+      get :edit, id: @item
+      response.should render_template :edit
+    end 
+  end
+
   describe "POST 'create'" do
     context "with valid attributes" do
       it "creates a new item" do
@@ -87,26 +109,90 @@ describe ItemController do
 
   describe "PUT 'update'" do
     context "with valid attributes" do
-      it "updates the specified item"
-      it "redirects to the show page for that item"
-      it "assigns @item as the updated item"
+      before(:each) do
+        @item = FactoryGirl.create(:item, description: "new desc")
+        put :update, id: @item, description: FactoryGirl.attributes_for(:item)
+      end
+
+      it "updates the specified item" do
+        @item.reload
+        @item.description.should eq "new desc"
+      end
+
+      it "redirects to the show page for that item" do
+        response.should redirect_to item_path(@item)
+      end
+
+      it "assigns @item as the updated item" do
+        assigns(:item).should eq @item
+      end
+
+      it "can only update an item that belongs to that user" do
+        user = FactoryGirl.create(:user_with_items)
+        user2 = FactoryGirl.create(:user_with_items)
+
+        sign_in user
+        item = user2.items.first
+        put :update, id: item.id, item: FactoryGirl.attributes_for(:item)
+        response.should redirect_to item_index_path
+      end
     end
 
     context "with invalid attributes" do
-      it "does not update the specified item"
-      it "rerenders the edit template"
-      it "assigns @item with the params passed"
+      before(:each) do
+        @item = FactoryGirl.create(:item)
+        @attrs = FactoryGirl.attributes_for(:item, description: "")
+        put :update, id: @item, item: @attrs
+      end
+
+      it "does not update the specified item" do
+        @item.reload
+        @item.description.should_not == ""
+      end
+
+      it "rerenders the edit template" do
+        response.should render_template :edit
+      end
+
+      it "assigns @item with the params passed" do
+        assigns(:item).should eq @item
+      end
     end
   end
 
   describe "DELETE 'destroy'" do
+    before(:each) do
+      @item = FactoryGirl.create(:item)
+    end
+
     context "belongs to user" do
-      it "deletes item if it exists"
-      it "redirects to the index"  
+      it "deletes item if it exists" do
+        expect {
+          delete :destroy, id: @item
+        }.to change(Item, :count).by(-1)
+      end
+
+      it "redirects to the index" do
+        delete :destroy, id: @item
+        response.should redirect_to item_index_path
+      end
     end
 
     context "does not belong to user" do
-      it "does not delete the item"
+      before(:each) do
+        @user = FactoryGirl.create(:user_with_items)
+      end
+
+      it "does not delete the item" do
+        expect {
+          delete :destroy, id: @user.items.first
+        }.to change(Item, :count).by(0)
+      end
+
+      it "redirects to the index page" do
+        delete :destroy, id: @user.items.first
+        response.should redirect_to item_index_path
+      end
     end
   end
 end
