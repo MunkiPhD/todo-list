347b24e0b9953de6179ead26bf66356dc5204255
MunkiPhD <munkiphd@gmail.com>
2012-10-14 21:39:12 -0400

    - updated styles - refactoring of styles

--- app/assets/javascripts/items.js
+++ app/assets/javascripts/items.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
   var list = $("#item-container");
+  // we want to make the list sortable, so use jQuery-UI's sortable() on doc ready
   $(list).sortable({
     placeholder: "ui-item-placeholder",
     cursor: 'move',
@@ -11,25 +12,33 @@ $(document).ready(function(){
   $("#item-container").disableSelection();
 });
 
+//
+// This function issues a post request to the server to update the sortable list
+// list = the list that has had sortable() executed on it
+//
 update_list = function(list) {
-	try {
-		var json_data = $(list).sortable('serialize');
-		$.ajax({
-			type: 'POST',
-			url: '/items/update_list',
-			data: json_data,
-			dataType: 'json',
-			success: function(data){
-				handle_response(data);
-			}
-		});
-	} catch (e) {
-		alert(e);
+  try {
+    var json_data = $(list).sortable('serialize');
+    $.ajax({
+	type: 'POST',
+        url: '/items/update_list',
+	data: json_data,
+	dataType: 'json',
+	success: function(data){
+	  handle_success_response(data);
 	}
-}
+    });
+  } catch (e) {
+    alert(e);
+  }
+};
 
-handle_response = function(data){
+//
+// Handles the successful response from the update list
+// data = the json data returned from the server
+//
+handle_success_response = function(data){
   $("#messages").addClass("success").html(data['message']).fadeIn(function(){
-	$("#messages").delay(1000).fadeOut(1000, "linear");		     
+    $("#messages").delay(1000).fadeOut(1000, "linear");		     
   });
-}
+};
